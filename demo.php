2c867fcd20d1146259f1e803d9d43b8777c8327b
iliailia1212 <iliailia1212@mail.ru>
2015-06-01 11:40:27 +0300

    Update demo.php

--- demo.php
+++ demo.php
@@ -22,6 +22,7 @@ $bd->update('test',//Обновляем таблцу ```test```
   ),
   array('name'=>'test3')//Обновляем имя на ```test3```
 );
+
 $bd->delete('test',//Удаляем данны из таблце ```test```
   array(//Фильтр
     array('id',1)//id = 1
@@ -39,4 +40,16 @@ print_r(
 );
 
 var_dump($bd->err());//Смотрим ошибки
+
+#Пример более сложного фильтра
+
+print_r(
+  $bd->read('test',//Получаем данны из таблци ```test```
+    array('*'),//Все колонки
+    array(//Фильтр
+     array('id',3,'<','OR'), //id < 3
+     array('name','test3'), //name = test3
+    )//Результат: `id` < 3 OR `name` = 'test3'
+  )
+);
 ?>
