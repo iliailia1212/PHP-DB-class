45c8c0275149fc840a285d3c22107126abb01b9d
iliailia1212 <iliailia1212@mail.ru>
2015-06-01 11:43:03 +0300

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,29 +1,29 @@
 <?php
 $cfg = array(
-	'bd' => array(
+	'db' => array(
 		'host' => 'localhost',
 		'user' => 'root',
 		'password' => '',
 		'name' => 'test_bd',
 	)
 );
-include_once('bd.class.php');
-$bd = new bd();
-$bd->connect($cfg);//Подключаемся
+include_once('db.class.php');
+$db = new db();
+$db->connect($cfg);//Подключаемся
 
-$bd->create('test',array(//Создаём запись в таблце ```test```
+$db->create('test',array(//Создаём запись в таблце ```test```
 	'name' => 'test2',//С данными name: test
 	'num' => 3//И num: 3
 ));
 
-$bd->update('test',//Обновляем таблцу ```test```
+$db->update('test',//Обновляем таблцу ```test```
   array(//Фильтр
     array('num', 3)// num = 3
   ),
   array('name'=>'test3')//Обновляем имя на ```test3```
 );
 
-$bd->delete('test',//Удаляем данны из таблце ```test```
+$db->delete('test',//Удаляем данны из таблце ```test```
   array(//Фильтр
     array('id',1)//id = 1
   )
@@ -31,7 +31,7 @@ $bd->delete('test',//Удаляем данны из таблце ```test```
 
 
 print_r(
-  $bd->read('test',//Получаем данны из таблци ```test```
+  $db->read('test',//Получаем данны из таблци ```test```
     array('*'),//Все колонки
     array(//Фильтр
      array('name','test3') //name = test3
@@ -39,12 +39,12 @@ print_r(
   )
 );
 
-var_dump($bd->err());//Смотрим ошибки
+var_dump($db->err());//Смотрим ошибки
 
 //Пример более сложного фильтра
 
 print_r(
-  $bd->read('test',//Получаем данны из таблци ```test```
+  $db->read('test',//Получаем данны из таблци ```test```
     array('*'),//Все колонки
     array(//Фильтр
      array('id',3,'<','OR'), //id < 3
